d6c641df5e3db19419cf240a06ffe607335354f1
Shin Mikami <m.shin0509@gmail.com>
2025-02-13 23:58:19 +0000

    feat(deploy.sh): CloudFrontキャッシュ削除要求を追加

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,12 @@
 
 S3_BUCKET="blog-mshin"
 BUILD_DIR="public/"
+DISTRIBUTION_ID="EAVL517Q9QH8O"
 
 # Upload to Amazon S3
 aws s3 sync $BUILD_DIR s3://$S3_BUCKET --delete
+echo "Deployment to S3 completed."
 
-echo "Deployment to S3 completed!"
+# Create invalidation
+aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/*"
+echo "Invalidation for CloudFront has been requested."
