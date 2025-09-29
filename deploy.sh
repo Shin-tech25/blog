51679e94df9ab0d27b0b0d262b6b464af8fd07eb
Shin Mikami <m.shin0509@gmail.com>
2025-09-29 14:00:00 +0900

    Refactor S3 sync commands in deployment script to improve asset upload process by clarifying excluded files and ensuring correct timestamp synchronization.

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,27 @@ BUILD_DIR="public/"
 DISTRIBUTION_ID="EAVL517Q9QH8O"
 
 # Upload to Amazon S3
+# 例外3ファイル（BUILD_DIR からの相対パスで記載）
+EX1="page-data/using-typescript/page-data.json"
+EX2="rss.xml"
+EX3="using-typescript/index.html"
+
+echo "Sync contents file updated the original size."
 aws s3 sync "$BUILD_DIR" "s3://$S3_BUCKET" \
   --delete \
-  --size-only
+  --size-only \
+  --exclude "$EX1" \
+  --exclude "$EX2" \
+  --exclude "$EX3"
+
+echo "Sync build timestamps."
+aws s3 sync "$BUILD_DIR" "s3://$S3_BUCKET" \
+  --delete \
+  --exclude "*" \
+  --include "$EX1" \
+  --include "$EX2" \
+  --include "$EX3"
+
 echo "Deployment to S3 completed."
 
 # Create invalidation
